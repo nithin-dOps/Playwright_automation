2f5755ed6912b1ff10b6974969cd53aae4b912ba
nithin-dOps <bandanithinreddy@gmail.com>
2026-07-15 12:23:01 +0200

    Update to screenshot helper

--- tests/smoke tests/login.spec.ts
+++ tests/smoke tests/login.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import loginData from '../../testdata/loginData.json';
 import {LoginPage} from '../../pages/LoginPage';
-
+import { takeScreenshot } from '../../utils/helper';
 test('Login Test', async ({ page }) => {
 
     const loginPage = new LoginPage(page);
@@ -12,7 +12,7 @@ test('Login Test', async ({ page }) => {
         loginData.email,
         loginData.password
     );
-
+    await takeScreenshot(page, 'login');
     await expect(page).toHaveTitle('My Account');
 
 });
